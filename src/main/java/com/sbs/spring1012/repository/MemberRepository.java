15b604a135a54f9122a7b1b1dbf318373116846d
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-08 01:29:32 +0900

    Update MemberRepository.java

--- src/main/java/com/sbs/spring1012/repository/MemberRepository.java
+++ src/main/java/com/sbs/spring1012/repository/MemberRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
-    Optional<Member> findByEmail(String email);
     boolean existsByEmail(String email);
-    Optional<Member> findByEmailAndPwd(String email,String pwd);
+    Optional<Member> findByAlias(String alias);
+    Optional<Member> findByEmailAndPwd(String email, String password);
+    Optional<Member> findByEmail(String email);
 
 }
