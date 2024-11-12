b5f8250b56766aaf9e48324a0fc697df8afb07f0
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-13 00:53:00 +0900

    Update Spring1012Application.java

--- src/main/java/com/sbs/spring1012/Spring1012Application.java
+++ src/main/java/com/sbs/spring1012/Spring1012Application.java
@@ -2,7 +2,7 @@ package com.sbs.spring1012;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableJpaAuditing
 @SpringBootApplication
 public class Spring1012Application {
 
